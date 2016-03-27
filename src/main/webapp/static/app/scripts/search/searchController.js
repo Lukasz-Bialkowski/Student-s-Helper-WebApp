0fb2dccee80253ad5706dfecd5e72a7ae54a9532
Piotr Kluska <p.kluska@outlook.com>
2016-03-27 23:14:37 +0200

    search box layout finished

--- src/main/webapp/static/app/scripts/search/searchController.js
+++ src/main/webapp/static/app/scripts/search/searchController.js
@@ -1,4 +1,11 @@
 /**
  * Created by piotrkluska on 26/03/16.
  */
-app.module()
+angular.module('app.module.SearchController',['mgcrea.ngStrap.typeahead'])
+  .controller('SearchController',['$scope', function ($scope) {
+    $scope.lecturers = ["Dr inz. Krzysztof Brzostowski ", "Dr inz Krzysztof Chudzik",
+                        "Dr inz. Krzysztof Juszczyszyn", "Dr inz Krzysztof Wasko"];
+    $scope.searchText = "";
+
+
+}]);
