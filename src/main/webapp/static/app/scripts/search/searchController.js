b7ddfd60282f7eb276098974d19d259a285460f8
Piotr Kluska <p.kluska@outlook.com>
2016-03-28 16:03:34 +0200

    navbar directive finished

--- src/main/webapp/static/app/scripts/search/searchController.js
+++ src/main/webapp/static/app/scripts/search/searchController.js
@@ -3,6 +3,7 @@
  */
 angular.module('app.module.SearchController',['mgcrea.ngStrap.typeahead'])
   .controller('SearchController',['$scope', function ($scope) {
+    console.info("fire");
     $scope.lecturers = ["Dr inż. Krzysztof Brzostowski ", "Dr inż. Krzysztof Chudzik",
       "Dr inż. Krzysztof Juszczyszyn", "Dr inż. Krzysztof Waśko"];
     $scope.searchText = "";
@@ -12,6 +13,14 @@ angular.module('app.module.SearchController',['mgcrea.ngStrap.typeahead'])
       restrict: 'E',
       replace : true,
       templateUrl: "scripts/search/mainPageSearch.tmpl.html",
+      controller: "SearchController"
+    }
+  })
+  .directive('navbarSearch',function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: "scripts/search/searchNavbar.tmpl.html",
       controller: "SearchController"
     }
   });
